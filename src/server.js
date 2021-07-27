191298a8db6b37fd4a499f28ff6ed679cc2e9b3a
h-hh19 <53941170+h-hh19@users.noreply.github.com>
2021-07-27 15:09:28 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,6 +4,18 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`)
+    next();
+}
+
+const handleHome = (req, res) => {
+return res.send("I like middlewares");
+}
+
+app.get("/",gossipMiddleware, handleHome);
+
+
 const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening)
